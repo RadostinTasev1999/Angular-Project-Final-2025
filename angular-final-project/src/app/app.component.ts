99514371b73b74fb0d8cc3dd4ff103f654e88090
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-07-21 20:25:30 +0300

    Clear initial html

--- angular-final-project/src/app/app.component.ts
+++ angular-final-project/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-final-project';
+  // title = 'angular-final-project';
 }
