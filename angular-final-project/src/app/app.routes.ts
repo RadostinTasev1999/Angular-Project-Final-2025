fdd30af473d9a86a3e9ebb561a99e19a24ee6645
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-07-21 20:26:30 +0300

    configure routing in app.routes.ts

--- angular-final-project/src/app/app.routes.ts
+++ angular-final-project/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'header', component: HeaderComponent },
+    { path: 'footer', component: FooterComponent}
+];
