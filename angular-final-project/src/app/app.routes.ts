41975febc4d3214fcda19e3589ed4479323a0d79
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-07-23 20:32:52 +0300

    added routing to posts component and profile component

--- angular-final-project/src/app/app.routes.ts
+++ angular-final-project/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
+import { PostsComponent } from './posts/posts/posts.component';
+import { ProfileComponent } from './user/profile/profile.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -11,5 +13,7 @@ export const routes: Routes = [
     { path: 'header', component: HeaderComponent },
     { path: 'footer', component: FooterComponent},
     { path: 'login', component: LoginComponent},
-    { path: 'register', component: RegisterComponent }
+    { path: 'register', component: RegisterComponent },
+    { path: 'posts', component: PostsComponent},
+    { path: 'profile', component: ProfileComponent}
 ];
