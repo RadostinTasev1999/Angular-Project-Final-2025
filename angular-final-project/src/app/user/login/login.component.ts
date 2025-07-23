ee48d7399c739cf3f44f1ffca7882e0d25b4e067
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-07-23 20:45:37 +0300

    declare onLogin method and import FormsModule

--- angular-final-project/src/app/user/login/login.component.ts
+++ angular-final-project/src/app/user/login/login.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  constructor(private userService: UserService){}
+
+    onLogin(form:NgForm){
+      if (form.invalid) {
+          return;
+      }
+
+      this.userService.login()
+
+      
+    }
 
 }
