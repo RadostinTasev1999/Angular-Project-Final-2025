68458584bd0550a03dcae7366c8418ee43aba48b
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-07-24 20:43:53 +0300

    created profile component
    Imported FormsModule
    Added Profile card html

--- angular-final-project/src/app/user/profile/profile.component.ts
+++ angular-final-project/src/app/user/profile/profile.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+
+  
 
 }
