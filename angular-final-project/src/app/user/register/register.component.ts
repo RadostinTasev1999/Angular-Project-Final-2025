279a9bb68f7bf5ec36c8276ba95f2e4ba4f40b5a
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-07-27 16:17:21 +0300

    Update Register Component
    Add new FormGroup instance and form controls
    declare onRegister method which handles form submission

--- angular-final-project/src/app/user/register/register.component.ts
+++ angular-final-project/src/app/user/register/register.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  registerForm = new FormGroup({
+    username: new FormControl('',[Validators.minLength(5), Validators.required]),
+    email: new FormControl('', [Validators.minLength(5), Validators.required]),
+    tel: new FormControl('', [Validators.required]),
+    passGroup: new FormGroup({
+        password: new FormControl('',[Validators.required, Validators.minLength(5)]),
+        rePassword: new FormControl('', [Validators.required])
+    })
+  })
+
+  constructor(private userService: UserService){}
+
+  onRegister(){
+    debugger
+    if (this.registerForm.invalid) {
+      return;
+    }
+    const {
+      email,
+      tel,
+      username,
+      passGroup: {password, rePassword} = {}
+    } = this.registerForm.value 
+
+    return this.userService.register(email!,username!,tel!,password!,rePassword!).subscribe((user) => {
+        console.log('User from DB is', user)
+    })
+ 
+  }
 
 }
