216ab867f473eeb4087f142f9f40c0e790b8e059
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-07-27 16:21:02 +0300

    update user service
    Injected httpClient via the component constructor
    declared register method, where we send a post request to the back-end api sending the user data
    
    created BehaviorSubject
    created observable with the BehaviorSubject as the source

--- angular-final-project/src/app/user/user.service.ts
+++ angular-final-project/src/app/user/user.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
 import { UserForAuth } from '../types/user';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
+  user$$ = new BehaviorSubject(null)
+  user$ = this.user$$.asObservable()
 
   USER_KEY = '[user]'
   user: UserForAuth | null = null;
@@ -13,16 +20,10 @@ export class UserService {
     return !!this.user
   }
 
-  constructor() {
-    try {
-      const lsUser = localStorage.getItem(this.USER_KEY) || '';
-      this.user = JSON.parse(lsUser)
-    } catch (error) {
-      this.user = null;
-    }
-   }
+  constructor(private http: HttpClient) {}
 
   login(){
+    const url = 'http://localhost:3000'
 
     this.user = {
       firstName: 'Radostin',
@@ -32,7 +33,28 @@ export class UserService {
       password: '123123123',
       _id: 'qwe645asd@jkw4412opq'
     }
-    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user))
+    
+  }
+
+  register(email: string, username: string, tel: string, password: string, rePassword: string){
+debugger
+    const payload = { 
+      email,
+      username,
+      tel,
+      password,
+      rePassword
+    }
+
+    const url = 'http://localhost:3000'
+debugger
+    return this.http.post(`${url}/users/register`, payload).pipe(
+      catchError( error => {
+        return throwError(error)
+      })
+
+    )
+              
   }
 
   logout(){
