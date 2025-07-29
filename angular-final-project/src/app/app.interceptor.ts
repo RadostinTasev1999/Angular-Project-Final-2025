636a655454cfc2a64ca4f68078e8fcb738faaf28
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-07-29 21:00:39 +0300

    Created app-interceptor

---
+++ angular-final-project/src/app/app.interceptor.ts
@@ -0,0 +1,39 @@
+import { HttpInterceptorFn } from "@angular/common/http";
+import { environment } from "./environment/environment.development";
+import { ErrorMsgService } from "./core/error-msg/error-msg.service";
+import { Router } from "@angular/router";
+import { inject } from "@angular/core";
+import { catchError } from "rxjs";
+
+const { apiUrl } = environment
+
+export const appInterceptor: HttpInterceptorFn = (req,next) => {
+
+    const API = '/api'
+
+    if (req.url.startsWith(API)) {
+        req = req.clone({
+            url: req.url.replace(API, apiUrl),
+            withCredentials: true
+        })
+    }
+
+    const errorMsgService = inject(ErrorMsgService)
+    const router = inject(Router)
+
+    return next(req)
+            .pipe(
+                catchError((err) => {
+                    if (err.status === 401) {
+                        router.navigate(['/login'])
+                    } else{
+                        errorMsgService.setError(err)
+                        router.navigate(['/error'])
+                    }
+
+                    return [err];
+                })
+            )
+
+
+}
