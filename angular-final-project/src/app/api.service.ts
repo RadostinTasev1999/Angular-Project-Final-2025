340b38c957df135d1a9628af1b283a2485943554
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-07-31 21:22:09 +0300

    added createPost method

--- angular-final-project/src/app/api.service.ts
+++ angular-final-project/src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from './environment/environment.development';
+import { Post } from './types/post';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +16,23 @@ export class ApiService {
     const { apiUrl } = environment
     // http://localhost:3000/api
 
-    return this.http.get(`${apiUrl}/posts`)
+    return this.http.get<Post[]>(`${apiUrl}/posts`)
     
+  }
+
+  createPost(theme:string, title: string, description: string, userId: string){
+
+    const { apiUrl } = environment
+
+    const payload = {
+      theme,
+      title,
+      description,
+      userId
+    }
+
+    return this.http.post<Post>(`${apiUrl}/posts`, payload)
+
   }
 
 
