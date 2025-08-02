8e96b02791cb30a65b1f352e06013f87a5cb5f36
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-08-02 20:29:02 +0300

    update Header Component
    injected router Service in the constructor
    added firstName getter method
    updated onLogout() method

--- angular-final-project/src/app/core/header/header.component.ts
+++ angular-final-project/src/app/core/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 import { UserService } from '../../user/user.service';
+
 
 @Component({
   selector: 'app-header',
@@ -10,16 +11,25 @@ import { UserService } from '../../user/user.service';
 })
 export class HeaderComponent {
 
-  constructor(private userService: UserService){}
+  constructor(private userService: UserService, private router: Router){}
 
   get isLoggedIn():boolean{
-      
+      console.log('User is logged in:', this.userService.isLoggedIn)
     return this.userService.isLoggedIn
+  }
+
+  get firstName():string | undefined{
+    
+    return this.userService.username
+
   }
 
   onLogout(){
     // call userService
-    this.userService.logout()
+    this.userService.logout().subscribe((message) => {
+        console.log(message)
+        this.router.navigate(['/home'])
+    })
   }
 
 }
