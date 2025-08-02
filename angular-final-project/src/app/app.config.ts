819e07c72a35911972aec033180d308883175fd2
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-08-02 20:26:46 +0300

    added withFetch() in provideHttpClient()

--- angular-final-project/src/app/app.config.ts
+++ angular-final-project/src/app/app.config.ts
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(
-      //  withFetch(),
+      withFetch(),
       withInterceptors([appInterceptor])
     )
   ]
