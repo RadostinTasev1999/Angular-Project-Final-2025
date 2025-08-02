bae0f743bcd7411756ce6fd934a74e9de806e334
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-08-02 20:52:36 +0300

    update profile component
    inject userService in the constructor
    create get firstName method to retrieve the logged in user username from userService
    create userData object and declare properties
    create isEditMode boolean property which we use to conditionally render HTML
    
    added profileForm model and delared formControls for the profile form
    
    create toggleEditMode() method to negate the boolean value of isEditMode property
    
    create onEdit() method to trigger rendering of edit form html
    
    create onSubmit method to manage form submission logic
    
    created onCancel() method to cancel form edit view
    
    on invoking lifecycle hook ngOnInit, update properties in the profileForm model with the properties of userData object

--- angular-final-project/src/app/user/profile/profile.component.ts
+++ angular-final-project/src/app/user/profile/profile.component.ts
@@ -1,14 +1,84 @@
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule, NgForm, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
+import { UserService } from '../user.service';
+import { UserForAuth } from '../../types/user';
+import { ProfileDetails } from '../../types/profile';
 
 @Component({
   selector: 'app-profile',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+
+  constructor(private userService: UserService){}
+
+  get firstName():string | undefined {
+
+    return this.userService.username
+
+  }
+
+  userData: ProfileDetails = {
+      username: 'Tasev',
+      position: 'Web Developer',
+      born: '04/19/1999',
+      city: 'Sofia',
+      country: 'Bulgaria',
+      email: 'tasev@gmail.com',
+      phoneNumber: '123-123-123'
+  }
+  
+  isEditMode: boolean = false;
+
+  profileForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    position: new FormControl('',[Validators.required]),
+    born: new FormControl('',[Validators.required]),
+    city: new FormControl('',[Validators.required]),
+    country: new FormControl('',[Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    phoneNumber: new FormControl('',[Validators.required])
+  })
+
+  ngOnInit(): void{
+    this.profileForm.patchValue(this.userData)
+  }
 
   
+  toggleEditMode(){
+    this.isEditMode = !this.isEditMode
+  }
+
+  onEdit(){
+    this.toggleEditMode()
+  }
+
+  onSubmit(){
+    if (this.profileForm.invalid) {
+        return;
+    }
+
+    // const {
+    //   username,
+    //   position,
+    //   born,
+    //   city,
+    //   country,
+    //   email,
+    //   phoneNumber
+    // } = this.profileForm.value
+
+    this.userData = this.profileForm?.value as ProfileDetails
+
+    this.toggleEditMode()
+
+  }
+
+  onCancel(){
+    this.toggleEditMode()
+  }
+
 
 }
