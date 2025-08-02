658524cfcd68e974d7179618620176e73ced803c
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-08-02 21:00:16 +0300

    update user.service.ts
    declare a BehaviorSubject
    declare a getter method to retrieve userId from user property
    declare username getter method to retrieve username property from user property
    inject httpService in the constructor
    subscribe to the Observable user$ and set the Observable's value to the user property
    
    create login method
    send userdata to the server and save the returned data from the response object in the behavior subject
    
    declare logout method to perform user logout and update BehaviorSubject value to null to chage application authentication state

--- angular-final-project/src/app/user/user.service.ts
+++ angular-final-project/src/app/user/user.service.ts
@@ -1,43 +1,61 @@
-import { Injectable } from '@angular/core';
-import { UserForAuth } from '../types/user';
+import { Injectable, OnInit } from '@angular/core';
+// import { UserForAuth } from '../types/user';
 import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs';
 import { BehaviorSubject } from 'rxjs';
 import { throwError } from 'rxjs';
+import { tap, Subscription } from 'rxjs'
+import { UserForAuth } from '../types/user';
+import { environment } from '../environment/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class UserService{
 
-  user$$ = new BehaviorSubject(null)
+  private user$$ = new BehaviorSubject<UserForAuth | null>(null)
   user$ = this.user$$.asObservable()
 
   USER_KEY = '[user]'
   user: UserForAuth | null = null;
+  userSubscription: Subscription | null = null;
+
 
   get isLoggedIn(): boolean{
+  //  console.log('User is:', this.user)
     return !!this.user
   }
 
-  constructor(private http: HttpClient) {}
+  get userId():string | undefined{
 
-  login(){
-    const url = 'http://localhost:3000'
+    return this.user?._id
+  }
 
-    this.user = {
-      firstName: 'Radostin',
-      lastName: 'Tasev',
-      email: 'radostintasev22@yahoo.com',
-      phoneNumber: '123-123-123',
-      password: '123123123',
-      _id: 'qwe645asd@jkw4412opq'
-    }
+  get username():string | undefined{
+
+    return this.user?.username
+
+  }
+
+  constructor(private http: HttpClient) {
+     this.user$.subscribe(user => {
+      this.user = user;
+    })
+  }
+
+ 
+
+  login(email: string, password: string){
+    
+    const API = '/api'
+
+    return this.http.post<UserForAuth>(`${API}/users/login`, { email, password })
+           .pipe(tap((user) => this.user$$.next(user)))
     
   }
 
   register(email: string, username: string, tel: string, password: string, rePassword: string){
-debugger
+
     const payload = { 
       email,
       username,
@@ -46,20 +64,21 @@ debugger
       rePassword
     }
 
-    const url = 'http://localhost:3000'
-debugger
-    return this.http.post(`${url}/users/register`, payload).pipe(
-      catchError( error => {
-        return throwError(error)
-      })
-
-    )
+    const API = '/api'
+    // debugger
+    return this.http.post<UserForAuth>(`${API}/users/register`, payload)
+              .pipe(tap(user => {
+                console.log('User is:', user)
+                this.user$$.next(user)
+              }))
               
   }
 
   logout(){
-    this.user = null;
-    localStorage.removeItem(this.USER_KEY)
+    const API = '/api'
+    
+    return this.http.post(`${API}/users/logout`,{})
+            .pipe(tap(() => this.user$$.next(null)))
   }
 
 }
