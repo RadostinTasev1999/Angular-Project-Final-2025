6d0185628a538e0872adfd76d34e952aca657a84
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-08-09 19:57:23 +0300

    Update post-details.component.ts
    imported FormsModule
    created isPostOwner and isEditMode properties
    
    injected router property in the constructor
    
    declared isAuthenticated getter method
    
    on subscription of getPostId method, I call getUser from userService to retrieve postOwner data.
    
    verified if logged in user is the post owner.

--- angular-final-project/src/app/posts/post-details/post-details.component.ts
+++ angular-final-project/src/app/posts/post-details/post-details.component.ts
@@ -4,10 +4,12 @@ import { Post } from '../../types/post';
 import { ApiService } from '../../api.service';
 import { UserService } from '../../user/user.service';
 import { UserForAuth } from '../../types/user';
+import { FormsModule,NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-details',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './post-details.component.html',
   styleUrl: './post-details.component.css'
 })
@@ -17,8 +19,17 @@ export class PostDetailsComponent implements OnInit {
   post = {} as Post
   postOwner:string = ''
   postCreator: UserForAuth | null = null
+  isPostOwner: boolean = false;
+  isEditMode: boolean = false;
+  
+  
 
-  constructor(private route: ActivatedRoute, private apiService: ApiService, private userService: UserService){}
+  constructor(private route: ActivatedRoute, private apiService: ApiService, private userService: UserService, private router: Router){}
+
+  get isAuthenticated(): boolean{
+
+    return this.userService.isLoggedIn
+  }
 
   ngOnInit(): void {
       this.route.params.subscribe((el) => {
@@ -31,13 +42,64 @@ export class PostDetailsComponent implements OnInit {
         console.log('Post is:', post)
         this.post = post
         this.postOwner = post.owner
+
+        // retrieve the postOwner data
+        this.userService.getUser(this.postOwner).subscribe((user) => {
+            this.postCreator = user
+        })
+
+        // verify if logged in user is the post owner
+        this.isPostOwner = this.userService.user?._id === this.postOwner
+
       })
 
-      this.userService.getUser(this.postOwner).subscribe((user) => {
-        this.postCreator = user
-        console.log('Post creator is:', user)
-      })
+          
 
   }
+
+  createComment(commentForm:NgForm){
+
+    if (commentForm?.invalid) {
+        return;
+    }
+
+    const {
+      email,
+      message,
+      name
+    } = commentForm.value
+      console.log('Comment form props are:', email, message, name)
+      debugger
+      const commentOwnerId = this.userService.user?._id
+      debugger
+      this.apiService.createPostComment(email,message,name,this.postId,commentOwnerId).subscribe((message) => {
+          console.log('Message is:',message)
+      })
+
+    commentForm.reset()
+  }
+
+  onDelete(){
+
+    const id = this.postId
+
+    this.apiService.deletePost(id).subscribe((message) => {
+     window.alert({message})
+    this.router.navigate(['/posts'])
+    })
+  }
+
+  onEdit(){
+    this.toggleEditMode()
+  }
+
+  onCancel(){
+    this.toggleEditMode()
+  }
+
+  toggleEditMode(){
+    this.isEditMode = !this.isEditMode
+  }
+
 
 }
