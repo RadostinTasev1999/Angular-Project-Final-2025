204c82306e80882256a53faf77b58843b70eb1f2
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-08-13 19:36:48 +0300

    Update login.component.ts
    imported EmailDirective
    declared domains property
    changed parameter name of onLogin method

--- angular-final-project/src/app/user/login/login.component.ts
+++ angular-final-project/src/app/user/login/login.component.ts
@@ -3,22 +3,27 @@ import { FormsModule } from '@angular/forms';
 import { NgForm } from '@angular/forms';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router'
+import { emailValidator } from '../../utils/email.validator';
+import { EmailDirective } from '../../directives/email.directive';
+import { DOMAINS } from '../../constants'
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, EmailDirective],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  domains = DOMAINS
+
   constructor(private userService: UserService, private router: Router){}
 
-    onLogin(form:NgForm){
-      if (form.invalid) {
+    onLogin(loginForm:NgForm){
+      if (loginForm.invalid) {
           return;
       }
-      const { email, password } = form.value
+      const { email, password } = loginForm.value
 
       this.userService.login(email,password).subscribe((user) => {
         console.log(`User ${user.username} has just logged in! `)
