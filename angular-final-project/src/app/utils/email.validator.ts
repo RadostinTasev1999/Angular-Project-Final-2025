b77b0bc2f2054e91397b0521c06000db5369da02
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-08-13 19:24:16 +0300

    Create email.validator.ts

---
+++ angular-final-project/src/app/utils/email.validator.ts
@@ -0,0 +1,14 @@
+import { ValidatorFn } from "@angular/forms"
+
+export function emailValidator(domains: string[]): ValidatorFn{
+    
+    const domainStr = domains.join('|')
+    const regExp = new RegExp(`[A-Za-z0-9]{6,}@gmail\.(${domainStr})`)
+
+    return (control) => {
+        const isValid = control.value === '' || regExp.test(control.value)
+
+        return isValid ? null : { emailValidator: true }
+    }
+
+}
