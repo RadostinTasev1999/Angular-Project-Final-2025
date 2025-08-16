6a7d2def47eaa628b60b878fd518441864c3afd0
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-08-16 23:17:52 +0300

    Update error-msg.service.ts
    declare error property
    change apiError$$ property to be public
    update setError method parameter type

--- angular-final-project/src/app/core/error-msg/error-msg.service.ts
+++ angular-final-project/src/app/core/error-msg/error-msg.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorMsgService {
 
-  private apiError$$ = new BehaviorSubject(null)
+  error: HttpErrorResponse | null = null
+
+  public apiError$$ = new BehaviorSubject(this.error)
   public apiError$ = this.apiError$$.asObservable()
 
-  setError(error:any): void{
+  setError(error:HttpErrorResponse): void{
     debugger
+    console.log('Error in error-msg.service.ts is', error)
     this.apiError$$.next(error)
   }
 }
