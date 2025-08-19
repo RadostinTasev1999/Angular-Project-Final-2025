0d05831840c3df7611daaf449704fce9a10cdb3e
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-08-19 21:52:05 +0300

    Clean up unused code

--- angular-final-project/src/app/shared/pipes/slice.pipe.ts
+++ angular-final-project/src/app/shared/pipes/slice.pipe.ts
@@ -7,12 +7,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SlicePipe implements PipeTransform {
 
   transform(value: string, maxCharCount: number): string {
-    debugger
+    
     const dots = value.length > maxCharCount ? '...' : '';
-    debugger
+    
     const result = value.substring(0, maxCharCount)
     const message = `<br><br><span class="text-primary">Click on details to read more</span>`
-    debugger
+    
     return `${result}${dots}${message}`;
   }
 
