31d0b3cb77b7836b0723e272d792609feeebc804
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-08-19 21:52:58 +0300

    Clean up unused code

--- angular-final-project/src/app/utils/match-passwords.validator.ts
+++ angular-final-project/src/app/utils/match-passwords.validator.ts
@@ -2,13 +2,13 @@ import { ValidatorFn } from "@angular/forms";
 
 export function matchPasswordsValidator(passwordControlName: string, rePasswordControlName: string): ValidatorFn{
     return (control) => {
-        debugger
+        
         const passwordFormControl = control.get(passwordControlName)
         const rePasswordFormControl = control.get(rePasswordControlName)
-        debugger
+        
         const passwordsAreMatching = 
             passwordFormControl?.value === rePasswordFormControl?.value;
-        debugger
+        
             return passwordsAreMatching ? null : { matchPasswordsValidator: true }
     }
 }
